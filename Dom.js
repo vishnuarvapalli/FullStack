386d0a9ffe5c03196a7cf4e991cfa591b0839356
vishnuarvapalli <vishnuarvapalli5@gmail.com>
2025-06-25 15:14:15 +0530

    commit

---
+++ Dom.js
@@ -0,0 +1,10 @@
+document.title="Dom in JS"
+document.body.style.backgroundColor="lightblue";
+document.getElementById("head1").innerHTML="Hello World!";
+document.getElementById("para1").innerHTML="Welcome to DOM Manuplator";
+document.getElementsByTagName("h1")[1].style.color="blue";
+document.getElementsByClassName("ds")[0].style.color="red";
+document.getElementsByClassName("ds")[1].style.color="blue";
+document.getElementsByClassName("ds")[2].style.color="yellow";
+document.getElementsByClassName("ds")[3].style.color="green";
+document.getElementsByClassName("ds")[0].font.style="italic";
